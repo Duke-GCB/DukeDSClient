1cdf53153d9100c342a2d909d72d0bbb8b5b153e
John Bradley <johnbradley2008@gmail.com>
2017-03-27 11:31:57 -0400

    cleanup for flake8

--- ddsc/tests/test_cmdparser.py
+++ ddsc/tests/test_cmdparser.py
@@ -3,18 +3,17 @@ from unittest import TestCase
 from ddsc.cmdparser import CommandParser
 
 
+def no_op():
+    pass
+
+
 class TestCommandParser(TestCase):
     def test_register_add_user_command(self):
         command_parser = CommandParser()
-        def call_func():
-            pass
-        command_parser.register_add_user_command(call_func)
+        command_parser.register_add_user_command(no_op)
         self.assertEqual(['add-user'], command_parser.subparsers.choices.keys())
 
     def test_register_remove_user_command(self):
         command_parser = CommandParser()
-        def call_func():
-            pass
-        command_parser.register_remove_user_command(call_func)
+        command_parser.register_remove_user_command(no_op)
         self.assertEqual(['remove-user'], command_parser.subparsers.choices.keys())
-
