62d44341d7da270702c9f8d2e8464c68fc81ac86
John Bradley <johnbradley2008@gmail.com>
2018-11-29 09:57:24 -0500

    call onpublish from circleci
    updates scripts to be circleci specific
    adds command to circle after deploying to pypi

--- deploy/onpublish.sh
+++ deploy/onpublish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Travis build trigger should have environment variables COMMIT_USER, COMMIT_EMAIL and COMMIT_TOKEN setup.
+# Circle build trigger should have environment variables COMMIT_USER, COMMIT_EMAIL and COMMIT_TOKEN setup.
 
 pip install requests
 python deploy/onpublish.py
