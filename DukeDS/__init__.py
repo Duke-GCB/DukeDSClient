d534b02d729d1fcbf6c6c025e78a1a75886c3726
John Bradley <johnbradley2008@gmail.com>
2019-05-29 11:08:46 -0400

    add can_deliver_to_user_with_username to DukeDS init

--- DukeDS/__init__.py
+++ DukeDS/__init__.py
@@ -7,7 +7,8 @@ download_file = DukeDS.download_file
 upload_file = DukeDS.upload_file
 delete_file = DukeDS.delete_file
 can_deliver_to_user_with_email = DukeDS.can_deliver_to_user_with_email
+can_deliver_to_user_with_username = DukeDS.can_deliver_to_user_with_username
 
 __all__ = ['list_projects', 'create_project', 'delete_project',
            'list_files', 'download_file', 'upload_file', 'delete_file',
-           'can_deliver_to_user_with_email']
+           'can_deliver_to_user_with_email', 'can_deliver_to_user_with_username']
