6d3623a706dfef02d97d70d8088245b3a5b9e684
John Bradley <johnbradley2008@gmail.com>
2021-05-18 09:31:30 -0400

    Create version 3.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='DukeDSClient',
-        version='3.2.0',
+        version='3.3.0',
         description='Command line tool(ddsclient) to upload/manage projects on the duke-data-service.',
         url='https://github.com/Duke-GCB/DukeDSClient',
         keywords='duke dds dukedataservice',
